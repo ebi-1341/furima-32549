6022ca223e096d09bd2f307206b7c2a52d2add35
ebi-1341 <megalogic89@gmail.com>
2020-12-14 19:38:54 +0900

    sessions.new.htmlのカラム名訂正及びストロングパラメーターの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "全角文字を使用してください" } do
-    validates :first_nama
+    validates :first_name
     validates :last_name
   end
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナを使用してください" } do
