7bcf732566a9a19290be042a91c447b7f6c181e7
ebi-1341 <megalogic89@gmail.com>
2020-12-15 17:01:42 +0900

    regionのActive_Hashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,9 +10,11 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :burden
+  belongs_to :region
 
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
   validates :burden_id, numericality: { other_than: 1 }
+  validates :region_id, numericality: { other_than: 1 }
   
 end
