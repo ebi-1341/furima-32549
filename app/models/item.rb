ba438c56ef4e263feedb10d3c3afe0a603f1db69
ebi-1341 <megalogic89@gmail.com>
2020-12-15 15:42:40 +0900

    バリデーションとアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :burden_id, presence: true
+  validates :region_id, presence: true
+  validates :day_id, presence: true
+
+  belongs_to :user
 end
