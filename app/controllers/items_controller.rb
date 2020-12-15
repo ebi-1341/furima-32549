dbe9ce2aec4de1d63cf013782bac4752fbfc5db1
ebi-1341 <megalogic89@gmail.com>
2020-12-15 18:29:52 +0900

    商品の投稿ができるようにパス指定、アクションの設定、ストロングパラメータの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path(@item)
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :description, :image, :category_id, :condition_id, :burden_id, :region_id, :day_id).merge(user_id: current_user.id)
   end
 end
