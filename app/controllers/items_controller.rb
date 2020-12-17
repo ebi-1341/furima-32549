e9327fb47c975ed1e38cb008fa02807e4ec3f30f
ebi-1341 <megalogic89@gmail.com>
2020-12-17 19:19:30 +0900

    編集ページでエラーメッセージを表示できるよう記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,12 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path(@item)
+    else
+      render :edit
+    end
   end
 
   private
