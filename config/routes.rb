c6935aa7b82dacfc11288b13aa4c592ca99de745
ebi-1341 <megalogic89@gmail.com>
2020-12-17 17:30:16 +0900

    誤って購入機能のテーブル、モデル、コントローラーを作成したため削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :show]
+  resources :items, only: [:new, :create, :show] do
+  end
 end
