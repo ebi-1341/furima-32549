a7e4f30bf904a0caeba3533e9dd6822476e8c0a2
ebi-1341 <megalogic89@gmail.com>
2020-12-21 15:52:31 +0900

    Formオブジェクトにバリデーションを記述

---
+++ app/models/buy.rb
@@ -0,0 +1,42 @@
+class buy
+  include ActiveModel::Model
+  attr_accessor :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, :name, :price, :description,
+  :category_id, :condition_id, :burden_id, :region_id, :day_id, :postcode, :city_name, :house_number, :building, :phone_number,
+  
+  ## addressesテーブルのバリデーション
+  validates :postcode, presence: true
+  validates :city_name, presence: true
+  validates :house_number, presence: true
+  validates :phone_number, presence: true
+  validates :purcahse, foreign_key: true
+  ## itemテーブルのバリデーション
+  with_options presence: true do
+    validates :name
+    validates :image
+    validates :price, format: { with: /\A\d[0-9]+\d[0-9]+\z/, message: '半角数字を入力してください' },
+                      inclusion: { in: 300..9_999_999, message: '金額は¥300から¥9,999,999までです' }
+    validates :description
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :burden_id
+    validates :region_id
+    validates :day_id
+  end
+  ## userテーブルのバリデーション
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  validates :birthday, presence: true
+
+  def save
+
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,4 @@
 class Item < ApplicationRecord
-  with_options presence: true do
-    validates :name
-    validates :image
-    validates :price, format: { with: /\A\d[0-9]+\d[0-9]+\z/, message: '半角数字を入力してください' },
-                      inclusion: { in: 300..9_999_999, message: '金額は¥300から¥9,999,999までです' }
-    validates :description
-  end
-
   belongs_to :user
   has_one_attached :image
   has_one :purchase
@@ -18,11 +10,4 @@ class Item < ApplicationRecord
   belongs_to :region
   belongs_to :day
 
-  with_options numericality: { other_than: 1 } do
-    validates :category_id
-    validates :condition_id
-    validates :burden_id
-    validates :region_id
-    validates :day_id
-  end
 end
