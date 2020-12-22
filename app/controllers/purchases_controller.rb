782f9497305dd8c76cd4797bb710013fc324133b
ebi-1341 <megalogic89@gmail.com>
2020-12-22 16:30:21 +0900

    Formオブジェクトを利用して購入機能実装の記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,24 @@
 class PurchasesController < ApplicationController
   def index
-    @purchase = BuyItem.new
+    @buy_item = BuyItem.new
     @item = Item.find(params[:item_id])
   end
 
 
   def create
+    @item = Item.find(params[:item_id])
+    @buy_item = BuyItem.new(purchase_params)
+    if @buy_item.valid?
+      @buy_item.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:buy_item).permit(:postcode, :region_id, :city_name, :house_number, :building, :phone_number, :hoge).merge(user_id: current_user.id, item_id: params[:item_id])
   end
   
 end
--- app/models/buy_item.rb
+++ app/models/buy_item.rb
@@ -1,18 +1,17 @@
 class BuyItem
 
   include ActiveModel::Model
-  attr_accessor :region_id, :postcode, :city_name, :house_number, :building, :phone_number
+  attr_accessor :region_id, :postcode, :city_name, :house_number, :building, :phone_number, :hoge, :user_id, :item_id
 
   with_options presence: true do
     validates :city_name
     validates :house_number
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "10桁か11桁の数字を入力してください"}
   end
 
-  validates :postcode, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :phone_number, presence: true, format: {with: /\A\d{10,11}\z/, message: "10桁か11桁の数字を入力してください"}
-
   def save
-    buy = Buy.create(user_id: user.id, item_id: item.id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
     Address.create(postcode: postcode, region_id: region_id, city_name: city_name, house_number: house_number, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 
