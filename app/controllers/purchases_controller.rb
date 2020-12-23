0663b9ce9b9f84fde4e1365f094a4cd635245586
ebi-1341 <megalogic89@gmail.com>
2020-12-23 12:28:18 +0900

    環境変数の記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,6 +9,7 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @buy_item = BuyItem.new(purchase_params)
     if @buy_item.valid?
+      pay_item
       @buy_item.save
       redirect_to root_path
     else
@@ -18,7 +19,16 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_params
-    params.require(:buy_item).permit(:postcode, :region_id, :city_name, :house_number, :building, :phone_number, :hoge).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:buy_item).permit(:postcode, :region_id, :city_name, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
   
 end
