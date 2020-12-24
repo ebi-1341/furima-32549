bbfde30a89de8739de184de885fd8cdfa1198f6b
ebi-1341 <megalogic89@gmail.com>
2020-12-24 17:54:49 +0900

    発見した不具合の修正(2)

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -36,7 +36,7 @@ class PurchasesController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id == @item.user_id || @item.id != @item.purchase.id
+    if current_user.id == @item.user_id || @item.purchase.present?
       redirect_to root_path
     end
   end
