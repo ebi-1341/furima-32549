bd43719b2b17d8d5136b1fbe3400670e57af42d9
ebi-1341 <megalogic89@gmail.com>
2020-12-24 11:38:06 +0900

    指摘箇所の修正(4)

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,6 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
-  before_action :set_item, only: [:index, :create, :move_to_index]
+  before_action :set_item, only: [:index, :create]
   before_action :move_to_index, only: [:index, :create]
 
   def index
